f1054bee9005e3c8e1c08073a4db58cbe92bacf6
mrStorrs <cj.storrs@gmail.com>
2022-02-02 22:21:58 -0800

    Tests not utilizing assertequals since all broke by switch to stream. Not enough time to fix. Use console for debug information.

--- test/FileHandlerTest.java
+++ test/FileHandlerTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.io.InputStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class FileHandlerTest {
 
