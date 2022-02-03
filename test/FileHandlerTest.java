d3c5b0ae96567776d65e292bc559998447ab89cd
mrStorrs <cj.storrs@gmail.com>
2022-02-02 22:10:43 -0800

    Switching to inputstream broke every test. Unfortunately I only have time to write these mini tests that don't even utilize assertequals.

--- test/FileHandlerTest.java
+++ test/FileHandlerTest.java
@@ -1,44 +1,24 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileHandlerTest {
 
-    @org.junit.jupiter.api.Test
-    void setPath() {
-        FileHandler a = new FileHandler("testpath", true);
-        a.setPath("new testpath");
-        assertEquals("new testpath", a.getPath());
-    }
+    static InputStream inputStream;
+    static FileHandler a;
 
-    @org.junit.jupiter.api.Test
-    void getPath() {
-        FileHandler a = new FileHandler("testpath", true);
-        assertEquals("testpath", a.getPath());
-    }
-
-    @org.junit.jupiter.api.Test
-    void readFile() {
-        FileHandler a = new FileHandler("./input/test.txt", true);
-        a.readFile();
-        assertEquals("HelloExpeditors", a.getRawInput().get(0));
+    @BeforeAll
+    public static void setUp(){
+        InputStream inputStream = Application.class.getResourceAsStream("INPUT.txt");
+        FileHandler a = new FileHandler(inputStream, true);
     }
 
     @Test
     void writeHouseholds() {
         // Test when given base input file
-        FileHandler a = new FileHandler("./input/INPUT.txt", true);
-        a.readFile();
-        a.writeHouseholds();
-    }
-
-    @Test
-    void writeHouseholdsDuplicate() {
-        //test for duplicate members
-        FileHandler a = new FileHandler("./input/testDuplicate.txt", true);
         a.readFile();
         a.writeHouseholds();
     }
