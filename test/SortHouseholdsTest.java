6fa633fb61eaeff3543c0d7485c2f8480fdc91cb
mrStorrs <cj.storrs@gmail.com>
2022-02-02 22:17:43 -0800

    Switching to input stream broke all tests. No time to fix. This is whats left. Please run individually and use console output for debugging.

--- test/SortHouseholdsTest.java
+++ test/SortHouseholdsTest.java
@@ -1,33 +1,41 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class SortHouseholdsTest {
+import java.io.InputStream;
 
+class SortHouseholdsTest {
+    static InputStream inputStream;
+    static FileHandler a;
+
+    @BeforeAll
+    public static void setUp(){
+        inputStream = Application.class.getResourceAsStream("INPUT.txt");
+        a = new FileHandler(inputStream, true);
+    }
+
+    /**
+     * The following tests must now be run individually until I can properly set them up.!!!
+     */
     @Test
     void sortHouseholds() {
-        FileHandler a = new FileHandler("./input/INPUT.txt", true);
         a.readFile();
         a.writeHouseholds();
-
         SortHouseholds sorter = new SortHouseholds() {};
         sorter.sortHouseholds(Household.getHouseholds(), true);
     }
 
     @Test
     void sortAllMembers() {
-        FileHandler a = new FileHandler("./input/INPUT.txt", true);
         a.readFile();
         a.writeHouseholds();
-
         SortHouseholds sorter = new SortHouseholds() {};
         sorter.sortAllMembers(true, 18, true);
     }
 
     @Test
     void sortAllMembersByHousehold() {
-        FileHandler a = new FileHandler("./input/INPUT.txt", true);
         a.readFile();
         a.writeHouseholds();
-
         SortHouseholds sorter = new SortHouseholds() {};
         sorter.sortAllMembersByHousehold(18, true);
     }
