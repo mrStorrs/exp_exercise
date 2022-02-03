df30c07f0e2ee3482553eeda7bf2f15bd0e71dc7
mrStorrs <cj.storrs@gmail.com>
2022-02-02 22:31:19 -0800

    Final cleanup before release. Fixed string compare.

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,9 @@
 import java.io.FileNotFoundException;  // error handling
+import java.io.InputStream;
 
 
 public class Application {
     public static void main(String[] args) {
-        Boolean DEBUG = false; //whether to output debug info or not.
         ConsoleGUI console = new ConsoleGUI();
         console.init();
 
@@ -12,13 +12,13 @@ public class Application {
     }
 
     public static void firstRuntime(String howToSort, Boolean DEBUG) throws FileNotFoundException {
-
-        FileHandler input = new FileHandler("input/INPUT.txt", DEBUG);
+        InputStream inputStream = Application.class.getResourceAsStream("INPUT.txt");
+        FileHandler input = new FileHandler(inputStream, DEBUG);
         input.readFile();
         input.writeHouseholds();
 
         SortHouseholds sorter = new SortHouseholds() {};
-        if(howToSort == "sortByMembers"){
+        if(howToSort.equals("sortByMembers")){
             sorter.sortAllMembers(true, 18, DEBUG);
         } else {
             sorter.sortAllMembersByHousehold(18, DEBUG);
@@ -26,11 +26,12 @@ public class Application {
     }
 
     public static void repeatRuntime(){
-         //clear objects for another go!
+        //clear objects for another go!
         HouseholdMember.clearMembers();
         System.out.println("\n\n\n-------- Clearing Members for another run! --------");
         Household.clearHouseholds();
         System.out.println("-------- Clearing Households for another run! --------\n\n\n");
+        //restart gui
         GUI myGui = new GUI();
         myGui.startGUI();
     }
